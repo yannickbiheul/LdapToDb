828443433a226c5ee108b412e2945d6a739a8593
BIHEUL Yannick <yannick.biheul@uhc-sante.fr>
2022-11-04 12:55:48 +0100

    balek

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -50,7 +50,13 @@ class TestController extends AbstractController
     #[Route('/test', name: 'app_test')]
     public function index(): Response
     {
-        // enregistrement des bâtiments
+        // Enregistrement de toutes les données "peopleRecord"
+        $this->recordManager->enregistrerTout();
+        // Enregistrement des hôpitaux
+        $this->hopitalManager->enregistrerHopitaux();
+        // Enregistrement des bâtiments
+        $this->batimentManager->enregistrerBatiments();
+        // Enregistrer les pôles
         $this->poleManager->enregistrerPoles();
 
         return $this->json([
